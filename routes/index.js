3bc58a4f0b70319b139892af44f531fc0ed1f736
Daniel Friedman <danielnfriedman@gmail.com>
2013-04-22 20:54:33 -0400

    Initial RGB-off functionality

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+
+// Configuration
+var USER_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN';  // https://a.ninja.is/hacking
+var RGBLEDGUID        = 'RGBLED_GUID';        // https://a.ninja.is/home
+
+
+// Instantiate a new ninja app
+var ninja = require('ninja-blocks').app({user_access_token:USER_ACCESS_TOKEN});
+// Create a container for these devices
+var DEVICES = {};
+
+
+// When we start up we fetch all of a users' devices.
+// This is so we don't have to fetch them on every Ninja callback.
+// Unfortunately it requires the app to be restarted to pull new devices.
+// You can put this into a setInterval to avoid this.
+ninja.devices(function(err,devices) {
+
+  DEVICES = devices;
+});
+
+
+/*
+ * Handle the callback from the Ninja platform
+ */
+exports.handleNinjaCallback = function(req, res){
+
+  // This little bit of code will turn your LED light off
+  // if it changes to anything but off.
+
+  if (req.body.GUID === RGBLEDGUID && req.body.DA !== "000000") {
+    ninja.device(RGBLEDGUID).actuate('000000');
+  }
+
+  // Very important to end the response.
+  res.end();
+};
